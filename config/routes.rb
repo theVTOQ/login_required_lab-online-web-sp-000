66ea1eb3912934632c8fffe3116095cad7cbc4c4
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-10-22 17:15:25 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/", to: "application#show"
+  get "/show", to: "secrets#show"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   post "/logout", to: "sessions#destroy"
