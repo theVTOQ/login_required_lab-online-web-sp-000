4440df1a0f9539910338d3cc00ab639aa981ed79
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-10-22 16:54:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def welcome
+  def show
     redirect_to "/login" if !logged_in?
   end
 end
